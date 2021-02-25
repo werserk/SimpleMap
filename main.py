9bbf3c57ab4d78b041f54776dd312c5a79a30eaf
werserk <werserk121@yandex.ru>
2021-02-25 23:48:02 +0300

    Поиск по объекту и метка

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import sys
 from PyQt5 import Qt, QtWidgets, QtCore
-from MapRequest import get_map_image
+from MapRequest import *
 
-KEY_LEFT = 65  # 16777234
-KEY_UP = 87  # 16777235
-KEY_RIGHT = 68  # 16777236
-KEY_DOWN = 83  # 16777237
+KEY_LEFT = 65, 16777234, 1060
+KEY_UP = 87, 16777235, 1062
+KEY_RIGHT = 68, 16777236, 1042
+KEY_DOWN = 83, 16777237, 1067
 
 
 class MainApp(QtWidgets.QWidget):
@@ -14,11 +14,14 @@ class MainApp(QtWidgets.QWidget):
 
         # Основные параметры
         self.map_size = 600  # Если слишком большой(маленький) размер каринки, то изменить здесь
-        self.x = 37.527256
-        self.y = 55.723587
+        self.x = 37.621094
+        self.y = 55.7536
         self.coords_for_dota = self.x, self.y
         self.z = 16
         self.map_type = 'map'
+        self.ind = 0
+        self.toponym = 'Красная площадь'
+        self.map_types = ['map', 'sat', 'sat,skl']
 
         # Инициализация окна
         self.initUI()
@@ -32,10 +35,20 @@ class MainApp(QtWidgets.QWidget):
         self.setStyleSheet('background: lightgray')
         self.setWindowTitle('Карты')
 
+        # Поиск объекта на карте
+        self.edt_find_toponym = QtWidgets.QLineEdit(self)
+        self.edt_find_toponym.setText(self.toponym)
+        self.edt_find_toponym.setGeometry(0, 0, 450, 30)
+        self.edt_find_toponym.setStyleSheet('background: white')
+
+        self.btn_find_toponym = QtWidgets.QPushButton(self)
+        self.btn_find_toponym.setText('Найти!')
+        self.btn_find_toponym.setGeometry(450, 0, 70, 30)
+        self.btn_find_toponym.clicked.connect(self.switch_toponym)
+
         # Изменение вида карты
-        self.map_types = ['map', 'sat', 'sat,skl']
         self.btn_box_map_types = QtWidgets.QPushButton(self)
-        self.btn_box_map_types.setText(self.map_type)
+        self.btn_box_map_types.setText('Тип')
         self.btn_box_map_types.setGeometry(self.width - 80, 0, 80, 30)
         self.btn_box_map_types.clicked.connect(self.switch_map_type)
 
@@ -56,9 +69,6 @@ class MainApp(QtWidgets.QWidget):
         self.lbl_z = QtWidgets.QLabel(self.frame_settings_for_search)
         self.lbl_z.setGeometry(200, 0, 100, 20)
 
-        # Отобразим картинку
-        self.search()
-
     def search(self):
         # Получаемся картинку
         pixmap = get_map_image((self.x, self.y), self.z, self.map_type, self.coords_for_dota)
@@ -76,12 +86,19 @@ class MainApp(QtWidgets.QWidget):
         self.frame_for_picture.setPixmap(pixmap)
 
     def switch_map_type(self):
-        # Находим нынешнюю карту и переключаем на следующую
-        text = self.btn_box_map_types.text()
-        ind = (self.map_types.index(text) + 1) % 3
-        new_text = self.map_types[ind]
-        self.btn_box_map_types.setText(new_text)
-        self.map_type = new_text
+        # Переключаем на следующую карту
+        self.ind = (self.ind + 1) % 3
+        self.map_type = self.map_types[self.ind]
+        self.search()
+
+    def switch_toponym(self):
+        self.toponym = self.edt_find_toponym.text()
+        response = get_map_coords(self.toponym)
+        if isinstance(response, str):
+            QtWidgets.QMessageBox.question(self, response)
+        else:
+            self.x, self.y = response
+            self.coords_for_dota = response
         self.search()
 
     def cmd_wheel_up(self):
@@ -97,30 +114,31 @@ class MainApp(QtWidgets.QWidget):
         self.search()
 
     def cmd_key_left(self):
-        self.x -= 1 / self.z ** 2
+        self.x -= self.z ** (-2)
         self.search()
 
     def cmd_key_right(self):
-        self.x += 1 / self.z ** 2
+        self.x += self.z ** (-2)
         self.search()
 
     def cmd_key_up(self):
-        self.y += 1 / self.z ** 2.4
+        self.y += self.z ** (-2.4)
         self.search()
 
     def cmd_key_down(self):
-        self.y -= 1 / self.z ** 2.4
+        self.y -= self.z ** (-2.4)
         self.search()
 
     def keyPressEvent(self, e):
+        print(e.key())
         if e.type() == QtCore.QEvent.KeyPress:
-            if e.key() == KEY_LEFT:
+            if e.key() in KEY_LEFT:
                 self.cmd_key_left()
-            if e.key() == KEY_RIGHT:
+            if e.key() in KEY_RIGHT:
                 self.cmd_key_right()
-            if e.key() == KEY_UP:
+            if e.key() in KEY_UP:
                 self.cmd_key_up()
-            if e.key() == KEY_DOWN:
+            if e.key() in KEY_DOWN:
                 self.cmd_key_down()
 
     def wheelEvent(self, e):
